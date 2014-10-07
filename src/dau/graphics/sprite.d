daf69bf7043aacf744334262f07939be83604afa
Ryan RC <murphyslaw480@gmail.com>
2014-10-06 22:06:02 -0400

    sprite code compiling

--- src/dau/graphics/sprite.d
+++ src/dau/graphics/sprite.d
@@ -1,35 +1,31 @@
 module dau.graphics.sprite;
 
 import std.string;
-import std.array;
 import std.conv;
-import std.algorithm : min;
-import dau.allegro;
-import dau.geometry.all;
-import dau.graphics.bitmap;
+import std.algorithm;
+import dau.engine;
+import dau.geometry.vector;
+import dau.graphics.texture;
 import dau.graphics.color;
 import dau.util.config;
 import dau.util.math;
 
 /// displays a single frame of a texture
 class Sprite {
-  this(string sheetName, string spriteName) {
-    _spriteSheet = loadSpriteSheet(sheetName);
-    auto data = _spriteData.entries[spriteName];
-    _name = spriteName;
-    _texture = getTexture(data["texture"]);
-    _row = to!int(data["row"]);
-    _col = to!int(data["col"]);
+  this(string textureName, string spriteName, Color tint = Color.white, float scale = 1) {
+    _texture = getTexture(textureName);
+    int idx = _texture.spriteIdx(spriteName);
+    _row = idx / _texture.numCols;
+    _col = idx % _texture.numCols;
     _tint = tint;
-    assert(_row >= 0 && _col >= 0 && _row < _texture.numRows && _col < _texture.numCols,
-        format("sprite coord %d, %d is out of bounds (%s)", _row, _col, spriteName));
-    _baseScale = to!int(data.get("baseScale", "1"));
+    _baseScale = scale;
   }
 
-  this(Texture spriteSheet, int spriteIdx, float baseScale = 1) {
+  this(Texture spriteSheet, int spriteIdx = 0, float baseScale = 1) {
     _texture = spriteSheet;
     _row = spriteIdx / _texture.numCols;
-    _col = spriteIdx % _texture.numCols; assert(_row >= 0 && _col >= 0 && _row < _texture.numRows && _col < _texture.numCols,
+    _col = spriteIdx % _texture.numCols;
+    assert(_row >= 0 && _col >= 0 && _row < _texture.numRows && _col < _texture.numCols,
         format("sprite coord %d, %d is out of bounds", _row, _col));
     _baseScale = baseScale;
   }
@@ -53,7 +49,7 @@ class Sprite {
   }
 
   void shift(Vector2i offset, float speed) {
-    _jiggleEffect = JiggleEffect(Vector2i.Zero, offset, speed, 1);
+    _jiggleEffect = JiggleEffect(Vector2i.zero, offset, speed, 1);
   }
 
   void update(float time) {
@@ -73,19 +69,19 @@ class Sprite {
 
   void draw(Vector2i pos) {
     auto adjustedPos = pos + _jiggleEffect.offset;
-    _texture.draw(_row, _col, adjustedPos, totalScale, _tint, _angle);
+    _texture.draw(adjustedPos, _row, _col, totalScale, _tint, _angle);
   }
 
   void draw(Vector2i pos, float scale) {
     auto adjustedPos = pos + _jiggleEffect.offset;
-    _texture.draw(_row, _col, adjustedPos, Vector2f(scale, scale), _tint, _angle);
+    _texture.draw(adjustedPos, _row, _col, Vector2f(scale, scale), _tint, _angle);
   }
 
   @property {
     /// width of the sprite after scaling (px)
-    int width() { return cast(int) (_spriteSheet.frameSize * totalScale.x); }
+    int width() { return cast(int) (_texture.frameWidth * totalScale.x); }
     /// height of the sprite after scaling (px)
-    int height() { return cast(int) (_spriteSheet.frameSize * totalScale.y); }
+    int height() { return cast(int) (_texture.frameHeight * totalScale.y); }
     /// width and height of sprite after scaling
     auto size() { return Vector2i(width, height); }
     /// tint color of the sprite
@@ -109,14 +105,14 @@ class Sprite {
   }
 
   protected:
-  Rect2i _region;
-  const SpriteSheet _spriteSheet;
+  int _row, _col;
 
   private:
+  Texture _texture;
   const float _baseScale;
   Vector2f _scaleFactor = Vector2f(1, 1);
-  float _angle = 0;
-  Color _tint = Color.white;
+  float _angle          = 0;
+  Color _tint           = Color.white;
 
   float _flashTimer, _totalFlashTime;
   Color[] _colorSpectrum;
@@ -125,6 +121,7 @@ class Sprite {
 }
 
 private:
+
 struct JiggleEffect {
   this(Vector2i start, Vector2i end, float frequency, int repetitions) {
     assert(frequency > 0, "cannot jiggle sprite with frequency <= 0");
@@ -147,7 +144,7 @@ struct JiggleEffect {
 
     Vector2i offset() {
       if (!active) {
-        return Vector2i.Zero;
+        return Vector2i.zero;
       }
       else if (_lerpFactor <= 1) {
         return lerp(_start, _end, _lerpFactor);
@@ -166,14 +163,4 @@ struct JiggleEffect {
   float _period;
   float _lerpFactor = 0;
   int _repetitions = 0;
-}
-
-private:
-class SpriteData {
-}
-
-ConfigData _spriteData;
-
-static this() {
-  _spriteData = loadConfigFile(Paths.spriteData);
 }
