616cc13073a9257cbd1e9ae4fed030b35de6826b
Ryan RC <murphyslaw480@gmail.com>
2014-10-07 22:32:12 -0400

    removal list

--- src/dau/util/removal_list.d
+++ src/dau/util/removal_list.d
@@ -6,7 +6,22 @@ import std.range;
 import std.container;
 
 /// list that automatically and efficiently removes entries for which cond(entry) is true
-struct RemovalList(T, cond) if (is(typeof(cond(T)) == bool)) {
+class RemovalList(T, alias cond) if (is(typeof(cond(T.init)) == bool)) {
+  void insert(T val) {
+    head = new Node(val, head);
+    if (head.next !is null) {
+      head.next.prev = head;
+    }
+  }
+
+  void clear() {
+    head = null;
+  }
+
+  Range opSlice() {
+    return new Range(head);
+  }
+
   private:
   Node head;
 
@@ -24,14 +39,13 @@ struct RemovalList(T, cond) if (is(typeof(cond(T)) == bool)) {
     T val;
     Node next, prev;
 
-    this(T val, Node next, Node prev) {
+    this(T val, Node next) {
       this.val = val;
       this.next = next;
-      this.prev = prev;
     }
   }
 
-  struct Range {
+  class Range {
     Node node;
 
     this(Node start) {
@@ -40,8 +54,8 @@ struct RemovalList(T, cond) if (is(typeof(cond(T)) == bool)) {
     }
 
     @property {
-      bool empty() { return node == null; }
-      T front() { return node; }
+      bool empty() { return node is null; }
+      T front() { return node.val; }
     }
 
     void popFront() {
@@ -51,11 +65,47 @@ struct RemovalList(T, cond) if (is(typeof(cond(T)) == bool)) {
 
     /// remove nodes for which cond is true starting from node
     void stripNodes() {
-      while(node !is null && cond(node)) {
+      while(node !is null && cond(node.val)) {
         auto next = node.next;
         removeNode(node);
         node = next;
       }
     }
   }
+}
+
+unittest {
+  class Foo {
+    this(int val, bool active) {
+      this.val = val;
+      this.active = active;
+    }
+
+    bool active;
+    int val;
+  }
+
+  auto list = new RemovalList!(Foo, x => !x.active);
+
+  list.insert(new Foo(1, false));
+  list.insert(new Foo(2, true));
+  list.insert(new Foo(3, true));
+  list.insert(new Foo(4, false));
+  list.insert(new Foo(5, false));
+
+  int[] vals;
+
+  foreach(el ; list) {
+    vals ~= el.val;
+    if (el.val == 2) { el.active = false; }
+  }
+  assert(vals == [3,2]);
+
+  list.insert(new Foo(6, true));
+
+  vals = [];
+  foreach(el ; list) {
+    vals ~= el.val;
+  }
+  assert(vals == [6, 3]);
 }
