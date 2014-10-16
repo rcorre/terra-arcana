4b91ce91897aacc18e392dbd995952898f4a1e0d
Ryan RC <murphyslaw480@gmail.com>
2014-10-16 10:23:58 -0400

    show hp bar in unit stats

--- src/dau/gui/pipbar.d
+++ src/dau/gui/pipbar.d
@@ -14,10 +14,10 @@ class PipBar : GUIElement {
     super(area);
     _maxVal = numPips;
     // insert pips, moving horizontally from left to right
-    auto pos = area.topLeft + pipSprite.size / 2;
+    auto pos = pipSprite.size / 2; // relative to top left
     for(int i = 0; i < numPips; ++i) {
       addChild(new Pip(pipSprite, pos));
-      pos.x += pipSprite.width / 2;
+      pos.x += pipSprite.width;
     }
   }
 
--- src/gui/unitinfo.d
+++ src/gui/unitinfo.d
@@ -6,7 +6,11 @@ import dau.gui.all;
 import model.unit;
 
 private enum {
-  spriteName = "gui/unit_status"
+  spriteName  = "gui/unit_status",
+  armorOffset = Vector2i(25, 21),
+  evadeOffset = Vector2i(224, 36),
+  hpArea      = Rect2i(56, 8, 141, 21),
+  apArea      = Rect2i(56, 24, 77, 21),
 }
 
 /// bar that displays progress as discrete elements (pips)
@@ -14,5 +18,11 @@ class UnitInfoGUI : GUIElement {
   this(Unit unit) {
     // TODO: choose corner of unit based on screen positioning
     super(new Sprite("gui/unit_status"), unit.area.bottomRight);
+    addChild(new PipBar(hpArea, unit.maxHp, new Sprite("gui/hpPip")));
   }
 }
+
+// TODO:
+// add pip bars to unitinfo
+// dau font
+// dau gui textbox
