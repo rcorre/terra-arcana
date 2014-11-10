71e5dbdf4bf5cd0a8687051e48f471f74bb4fb82
Ryan <ryan@rcorre.net>
2014-11-09 19:56:44 -0500

    fix compile error in considermove

--- src/battle/state/considermove.d
+++ src/battle/state/considermove.d
@@ -45,7 +45,7 @@ class ConsiderMove : State!Battle {
           _path = _pathFinder.pathToward(tile);
         }
       }
-      if (input.select && _path !is null && _path !is empty) {
+      if (input.select && _path !is null && !_path.empty) {
         b.states.pushState(new MoveUnit(_unit, _path));
       }
       else if (input.altSelect) {
