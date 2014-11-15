3a2e49366b644000d4cadf771c1af3d980e8c03d
Ryan <ryan@rcorre.net>
2014-11-15 12:04:15 -0500

    add animated sprites to deploy menu

--- src/gui/deploymenu.d
+++ src/gui/deploymenu.d
@@ -6,16 +6,15 @@ import dau.all;
 import model.all;
 
 private enum {
-  bgName            = "gui/deploy_menu",
-  buttonName        = "gui/deploy_button",
-  firstButtonOffset = Vector2i(0, 22),
+  iconFmt = "icon_%s"
 }
 
 /// bar that displays progress as discrete elements (pips)
 class DeployMenu : Menu!(string, DeployButton) {
   this(const Faction faction, Vector2i offset, DeployButton.Action onClick, int cp) {
-    // TODO: choose corner of unit based on screen positioning
-    super(getGUIData("deployMenu"), offset, firstButtonOffset, onClick);
+    auto menuData = getGUIData("deployMenu");
+    auto firstButtonOffset = menuData["firstButtonOffset"].parseVector!int;
+    super(menuData, offset, firstButtonOffset, onClick);
     auto buttonData = data.child["deployButton"];
     foreach(key ; faction.standardUnitKeys) {
       bool enabled = getUnitData(key).deployCost <= cp;
@@ -30,37 +29,28 @@ class DeployButton : MenuButton!string {
     _dullShade = data["dullShade"].parseColor;
     _brightShade = data["brightShade"].parseColor;
     _disabledShade = data["disabledShade"].parseColor;
-    _spriteOffset = data["spriteOffset"].parseVector!int;
     auto unit = getUnitData(unitKey);
-    //addChild(new Icon(_animation, spriteOffset));
     addChild(new TextBox(data.child["text"], unit.name, data["nameOffset"].parseVector!int));
     addChild(new TextBox(data.child["text"], unit.deployCost, data["costOffset"].parseVector!int));
     sprite.tint = enabled ? _dullShade : _disabledShade;
-    _animation = new Animation(unitKey, "idle", Animation.Repeat.loop);
+    auto spriteOffset = data["spriteOffset"].parseVector!int;
+    auto iconData = getGUIData(iconFmt.format(unitKey));
+    addChild = new Icon(iconData, spriteOffset);
   }
 
   override void onMouseEnter() {
     if (_enabled) {
       sprite.tint = _brightShade;
-      _animation.start();
     }
   }
 
   override void onMouseLeave() {
     if (_enabled) {
       sprite.tint = _dullShade;
-      _animation.stop();
     }
   }
 
-  override void draw(Vector2i parentTopLeft) {
-    super.draw(parentTopLeft);
-    _animation.draw(area.topLeft + parentTopLeft);
-  }
-
   private:
-  Animation _animation;
-  Vector2i _spriteOffset;
   Color _dullShade, _brightShade, _disabledShade;
 }
 
