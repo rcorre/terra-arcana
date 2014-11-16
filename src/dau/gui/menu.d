a4ad02d65dc7ff7faf576266f83259bcac2c7cf9
Ryan <ryan@rcorre.net>
2014-11-15 21:11:47 -0500

    refactor gui menu

--- src/dau/gui/menu.d
+++ src/dau/gui/menu.d
@@ -1,6 +1,6 @@
 module dau.gui.menu;
 
-import std.algorithm;
+import std.algorithm, std.conv;
 import dau.geometry.all;
 import dau.graphics.all;
 import dau.gui.element;
@@ -8,20 +8,22 @@ import dau.gui.data;
 import dau.util.removal_list;
 
 abstract class Menu(EntryType, ButtonType) : GUIElement {
-  this(GUIData data, Vector2i pos, Vector2i firstButtonOffset, ButtonType.Action onClick) {
+  this(GUIData data, Vector2i pos, ButtonType.Action onClick) {
     super(data, pos, Anchor.topLeft);
-    _nextButtonOffset = firstButtonOffset;
+    _nextButtonOffset = data.get("firstButtonOffset", "0,0").parseVector!int;
+    _menuSpacingY = data.get("menuSpacingY", "0").to!int;
     _onClick = onClick;
   }
 
   protected void addEntry(GUIData data, EntryType entry, bool enabled = true) {
     auto button = new ButtonType(data, entry, _nextButtonOffset, _onClick, enabled);
     addChild(button);
-    _nextButtonOffset.y += button.height;
+    _nextButtonOffset.y += button.height + _menuSpacingY;
   }
 
   private:
   Vector2i _nextButtonOffset;
+  int _menuSpacingY;
   ButtonType.Action _onClick;
 }
 
@@ -29,9 +31,13 @@ abstract class MenuButton(EntryType) : GUIElement {
   alias Action = void delegate(EntryType);
   this(GUIData data, EntryType entry, Vector2i pos, Action onClick, bool enabled) {
     super(data, pos, Anchor.topLeft);
+    _dullShade = data["dullShade"].parseColor;
+    _brightShade = data["brightShade"].parseColor;
+    _disabledShade = data["disabledShade"].parseColor;
     _onClick = onClick;
     _value = entry;
     _enabled = enabled;
+    sprite.tint = enabled ? _dullShade : _disabledShade;
   }
 
   override bool onClick() {
@@ -41,6 +47,21 @@ abstract class MenuButton(EntryType) : GUIElement {
     }
     return false;
   }
+
+  override void onMouseEnter() {
+    if (_enabled) {
+      sprite.tint = _brightShade;
+    }
+  }
+
+  override void onMouseLeave() {
+    if (_enabled) {
+      sprite.tint = _dullShade;
+    }
+  }
+
+  private:
+  Color _dullShade, _brightShade, _disabledShade;
 
   protected:
   bool _enabled;
--- src/gui/deploymenu.d
+++ src/gui/deploymenu.d
@@ -13,8 +13,7 @@ private enum {
 class DeployMenu : Menu!(string, DeployButton) {
   this(const Faction faction, Vector2i offset, DeployButton.Action onClick, int cp) {
     auto menuData = getGUIData("deployMenu");
-    auto firstButtonOffset = menuData["firstButtonOffset"].parseVector!int;
-    super(menuData, offset, firstButtonOffset, onClick);
+    super(menuData, offset, onClick);
     auto buttonData = data.child["deployButton"];
     foreach(key ; faction.standardUnitKeys) {
       bool enabled = getUnitData(key).deployCost <= cp;
@@ -26,32 +25,13 @@ class DeployMenu : Menu!(string, DeployButton) {
 class DeployButton : MenuButton!string {
   this(GUIData data, string unitKey, Vector2i pos, Action onClick, bool enabled) {
     super(data, unitKey, pos, onClick, enabled);
-    _dullShade = data["dullShade"].parseColor;
-    _brightShade = data["brightShade"].parseColor;
-    _disabledShade = data["disabledShade"].parseColor;
     auto unit = getUnitData(unitKey);
     addChild(new TextBox(data.child["text"], unit.name, data["nameOffset"].parseVector!int));
     addChild(new TextBox(data.child["text"], unit.deployCost, data["costOffset"].parseVector!int));
-    sprite.tint = enabled ? _dullShade : _disabledShade;
     auto spriteOffset = data["spriteOffset"].parseVector!int;
     auto iconData = getGUIData(iconFmt.format(unitKey));
     addChild = new Icon(iconData, spriteOffset);
   }
-
-  override void onMouseEnter() {
-    if (_enabled) {
-      sprite.tint = _brightShade;
-    }
-  }
-
-  override void onMouseLeave() {
-    if (_enabled) {
-      sprite.tint = _dullShade;
-    }
-  }
-
-  private:
-  Color _dullShade, _brightShade, _disabledShade;
 }
 
 Font _font;
