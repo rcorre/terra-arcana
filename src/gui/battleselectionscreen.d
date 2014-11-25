407aa7bdd2eb90a5e10df0b0b767505b076e9690
Ryan <ryan@rcorre.net>
2014-11-25 07:48:36 -0500

    change choosebattle screen appearance

--- src/gui/battleselectionscreen.d
+++ src/gui/battleselectionscreen.d
@@ -14,36 +14,38 @@ enum mapFormat = Paths.mapDir ~ "/%s.json";
 class BattleSelectionScreen : GUIElement {
   this(Title title) {
     super(getGUIData("selectBattle"), Vector2i.zero);
-      auto playerOffset       = data["playerOffset"].parseVector!int;
-      auto pcOffset           = data["pcOffset"].parseVector!int;
-      auto factionTitleOffset = data["factionTitleOffset"].parseVector!int;
-      auto playerTitleOffset  = data["playerTitleOffset"].parseVector!int;
-      auto pcTitleOffset      = data["pcTitleOffset"].parseVector!int;
-      auto startButtonOffset  = data["startButtonOffset"].parseVector!int;
-      auto mapOffset          = data["mapSelectOffset"].parseVector!int;
 
-      auto mapPaths = Paths.mapDir.dirEntries("*.json", SpanMode.shallow);
-      auto mapNames = mapPaths.map!(x => x.baseName(".json"));
+    auto mapPaths = Paths.mapDir.dirEntries("*.json", SpanMode.shallow);
+    auto mapNames = mapPaths.map!(x => x.baseName(".json"));
 
-      addChild(new TextBox(data.child["playerText"], "Player", playerTitleOffset));
-      addChild(new TextBox(data.child["playerText"], "PC", pcTitleOffset));
-      addChild(new TextBox(data.child["factionText"], "Faction", factionTitleOffset));
+    _startButton = new Button(data.child["startButton"], &startBattle);
+    _startButton.enabled = false;
 
-      _startButton = new Button(data.child["startButton"], startButtonOffset, &startBattle);
-      _playerFactionMenu = new FactionMenu(playerOffset, &selectPlayerFaction);
-      _pcFactionMenu     = new FactionMenu(pcOffset, &selectPCFaction);
-      _mapSelector = new StringSelection(getGUIData("selectMap"), mapOffset, mapNames.array);
+    // map an faction selections
+    auto mapOffset          = data["mapSelectOffset"].parseVector!int;
+    auto selfFactionOffset  = data["selfFactionOffset"].parseVector!int;
+    auto otherFactionOffset = data["otherFactionOffset"].parseVector!int;
 
-      addChildren(_startButton, _playerFactionMenu, _pcFactionMenu, _mapSelector);
-      _startButton.enabled = false;
+    _playerFactionMenu = new FactionMenu(selfFactionOffset, &selectPlayerFaction);
+    _pcFactionMenu     = new FactionMenu(otherFactionOffset, &selectPCFaction);
+    _mapSelector       = new StringSelection(getGUIData("selectMap"), mapOffset, mapNames.array);
+    addChildren(_startButton, _playerFactionMenu, _pcFactionMenu, _mapSelector);
 
-      addChild(new Button(data.child["backButton"], () => title.states.setState(new ShowTitle)));
+    _messageBox = new MessageBox(data.child["messageBox"]);
+    _messageInput = new TextInput(data.child["messageInput"], &postMessage);
+    addChildren(_messageBox, _messageInput);
+
+    addChild(new Button(data.child["backButton"], () => title.states.setState(new ShowTitle)));
+
+    addChildren!TextBox("titleText", "subtitle");
   }
 
   private:
   FactionMenu _playerFactionMenu, _pcFactionMenu;
   StringSelection _mapSelector;
   Button _startButton;
+  MessageBox _messageBox;
+  TextInput _messageInput;
 
   void selectPlayerFaction(Faction faction) {
     if (_pcFactionMenu.selection == faction) {
@@ -67,5 +69,8 @@ class BattleSelectionScreen : GUIElement {
   }
 
   void backToMenu() {
+  }
+
+  void postMessage(string text) {
   }
 }
