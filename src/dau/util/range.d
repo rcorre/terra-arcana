3ce82c64116d9744a8dd85bd150db8d2b9b3e40f
Ryan <ryan@rcorre.net>
2014-12-04 10:06:04 -0500

    add mapDict helper

--- src/dau/util/range.d
+++ src/dau/util/range.d
@@ -14,3 +14,23 @@ unittest {
   assert(a.find!(x => x > 3).frontOr(0) == 4);
   assert(a.find!(x => x > 5).frontOr(0) == 0);
 }
+
+private enum bool mapsToSelf(alias fun, T) = is(typeof(fun(T.init)) : T);
+
+auto mapDict(alias keyFun, alias valFun, K, V)(V[K] aa) if 
+  (mapsToSelf!(keyFun, K) && mapsToSelf!(valFun, V))
+{
+  V[K] result;
+  foreach(k, v ; aa) {
+    auto key = keyFun(k);
+    auto val = valFun(v);
+    result[key] = val;
+  }
+  return result;
+}
+
+unittest {
+  auto aa = ["a": 1, "b": 2, "c": 3];
+  auto aa1 = aa.mapDict!(x => x ~ "x", x => x + 3);
+  assert(aa1 == ["ax": 4, "bx": 5, "cx": 6]);
+}
