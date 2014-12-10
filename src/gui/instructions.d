4d187c061e4f9aff4e6ce639b585e9c575837d6a
Ryan <ryan@rcorre.net>
2014-12-09 23:01:07 -0500

    set up instructions screen

--- src/gui/instructions.d
+++ src/gui/instructions.d
@@ -1,38 +1,50 @@
 module gui.instructions;
 
-import std.string;
+import std.string, std.conv;
 import dau.all;
 import model.all;
 import title.title;
 import title.state.showtitle;
 
-private enum pageFormat = "page%d";
+private enum textureFormat = "gui/instructions%d";
+private enum pageDisplayFormat = "Page %d / %d";
 
 class InstructionScreen : GUIElement {
   this(Title title) {
-    super(getGUIData("instructions"), Vector2i.zero);
+    super(getGUIData("instructions"), Rect2i(0, 0, Settings.screenW, Settings.screenH));
 
-    setPage(2);
-
+    _numPages = data["numPages"].to!int;
     _title = title;
+    setPage(1);
   }
 
   private:
   Title _title;
+  GUIElement _currentPage;
   int _pageNum;
+  int _numPages;
 
   void setPage(int pageNum) {
     _pageNum = pageNum;
-    clear();
-    string name = pageFormat.format(pageNum);
-    auto page = data.child[name];
 
-    foreach(textData ; page.child["textBoxes"].child) {
-      addChild(new TextBox(textData));
+    if (_currentPage !is null) {
+      _currentPage.active = false;
     }
 
-    foreach(iconData ; page.child["icons"].child) {
-      addChild(new Icon(iconData));
+    auto pageData = new GUIData;
+    pageData["texture"] = textureFormat.format(pageNum);
+    _currentPage = addChild(new GUIElement(pageData, Vector2i.zero));
+
+    // navigation buttons
+    if (_pageNum > 1) {
+      _currentPage.addChild(new Button(data.child["prevPage"], { setPage(_pageNum - 1); }));
     }
+    if (_pageNum < _numPages) {
+      _currentPage.addChild(new Button(data.child["nextPage"], { setPage(_pageNum + 1); }));
+    }
+
+    // set up subject and page number textboxes
+    string pageText = pageDisplayFormat.format(pageNum, _numPages);
+    _currentPage.addChild(new TextBox(data.child["pageNumber"], pageText));
   }
 }
