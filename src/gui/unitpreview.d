723b22927a0d081317133275355910bebe96d9c6
Ryan <ryan@rcorre.net>
2014-12-13 22:42:07 -0500

    set unitpreview max pips on refresh

--- src/gui/unitpreview.d
+++ src/gui/unitpreview.d
@@ -11,16 +11,26 @@ class UnitPreview : DynamicGUIElement {
     super(getGUIData("unitPreview"));
 
     _name = addChild(new TextBox(data.child["name"], unit.name));
-    _hpBar = addChild(new PipBar(data.child["hpBar"], unit.maxHp));
-    _apBar = addChild(new PipBar(data.child["apBar"], unit.maxAp));
 
     refresh(unit);
   }
 
   void refresh(Unit unit) {
     _name.text = unit.name;
+
+    if (_hpBar !is null) {
+      _hpBar.active = false;
+    }
+    if (_apBar !is null) {
+      _apBar.active = false;
+    }
+
+    _hpBar = addChild(new PipBar(data.child["hpBar"], unit.maxHp));
+    _apBar = addChild(new PipBar(data.child["apBar"], unit.maxAp));
+
     _hpBar.setVal(unit.hp);
     _apBar.setVal(unit.ap);
+
     transitionActive = true;
   }
 
