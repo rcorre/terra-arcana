ae2cd60a10b5d123b588fc614b2c0752f2ac78cd
Ryan <ryan@rcorre.net>
2014-12-21 18:06:40 -0500

    create single package

--- tools/package.sh
+++ tools/package.sh
@@ -2,8 +2,7 @@
 
 NAME=terra-arcana
 VERSION=0_0
-CONTENT="$NAME/"
-BINARY=""
 
-tar -C .. -cvzf "$NAME$VERSION.tar.gz" $NAME/{terra-arcana,data,LICENSE,README.md}
-tar -cvzf "$NAME-content$VERSION.tar.gz" content/ LICENSE README.md
+tar -C .. -cvzf "$NAME$VERSION.tar.gz" $NAME/{terra-arcana,data,content,LICENSE,README.md}
+
+#tar -cvzf "$NAME-content$VERSION.tar.gz" content/ LICENSE README.md
